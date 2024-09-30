193e6a37986f87fb2ada2a466254e0ecbdf54190
NewtonMutugi <newtonmut197@gmail.com>
2024-09-30 21:19:57 +0300

    Cleaning unnecessary files

--- src/routes/index.js
+++
@@ -1,6 +0,0 @@
-import { Navigate, useRoutes } from 'react-router-dom';
-import MainRoutes from './MainRoutes';
-
-export default function ThemeRoutes() {
-  return useRoutes([{ path: '/', element: <Navigate to="/" /> }, MainRoutes]);
-}
