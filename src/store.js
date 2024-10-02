7fe1337c1c65d13914ad1377e9fb799387d8dfe5
NewtonMutugi <newtonmut197@gmail.com>
2024-10-02 17:38:38 +0300

    Refactor store.js to use legacy_createStore for compatibility with Redux

--- src/store.js
+++ src/store.js
@@ -1,4 +1,8 @@
-import { compose, createStore, applyMiddleware } from 'redux';
+import {
+  compose,
+  legacy_createStore as createStore,
+  applyMiddleware,
+} from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { thunk } from 'redux-thunk';
